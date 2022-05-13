70e3993c18d80fcf2581922ce6abbcd837439808
Joel Tolvanen <joel.tolvanen@xiple.io>
2022-05-13 19:52:16 +0300

    refactor: verry ddynamic values

--- karioke.js
+++ karioke.js
@@ -5,15 +5,16 @@ const song = "kukkuu";
 const { lyrics } = require(`./${song}.json`);
 const { lines } = lyrics.singers[0];
 
-lines.map(({ words }) => {
+lines.forEach(({ words }) => {
   words.map(({ syllables }) => {
     const word = syllables.map(({ text }) => text).join("");
     const { start } = syllables[0];
+    const offset = 2000;
 
-    setTimeout(() => console.log(word), start);
+    setTimeout(() => console.log(word), start-offset);
   });
 });
 
-player.play("kukkuu.mp3", function (err) {
+player.play(`${song}.mp3`, function (err) {
   if (err) throw err;
 });
