716ad03f99b84798877cfbfcb5c8fd4f919baf93
rennehir <renne@rennehir.fi>
2022-05-14 00:07:32 +0300

    feat: Add songs to queue

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -1,15 +1,27 @@
-import { Box } from 'ink';
+import React, { useState } from 'react';
+import { Box, Text, useInput } from 'ink';
 import SelectInput from 'ink-select-input';
-import React from 'react';
+import TextInput from 'ink-text-input';
+
 import { getFileNames } from '../lib/helpers';
+import { useQueue } from '../context/queue';
 
 interface Item {
   label: string;
   value: string;
 }
 
-const SongList = () => {
+interface SongListProps {
+  onFinish: () => void;
+}
+
+const SongList = (props: SongListProps) => {
   const songs = getFileNames();
+  const { queue, addToQueue } = useQueue();
+
+  const [performerName, setPerformerName] = useState<string>('');
+  const [selectedSong, setSelectedSong] = useState<string>('');
+
   const selectItems: Item[] = songs.map((song) => {
     return {
       label: song.split('assets/')[1].split('.')[0],
@@ -18,12 +30,32 @@ const SongList = () => {
   });
 
   const handleSelect = (item: Item) => {
-    console.log('kukkuu');
+    setSelectedSong(item.value);
   };
+
+  useInput((input, key) => {
+    if (key.return && selectedSong && performerName) {
+      addToQueue({ performerName });
+      setSelectedSong('');
+      setPerformerName('');
+      props.onFinish();
+    }
+  });
+
+  if (!selectedSong) {
+    return (
+      <Box>
+        <SelectInput items={selectItems} onSelect={handleSelect} />
+      </Box>
+    );
+  }
 
   return (
     <Box>
-      <SelectInput items={selectItems} onSelect={handleSelect} />
+      <Box marginRight={1}>
+        <Text>Enter your name:</Text>
+      </Box>
+      <TextInput value={performerName} onChange={setPerformerName} />
     </Box>
   );
 };
