85aad004e8bf7981474e49a1e3e46a27399a1565
Wezqu <vpuotila@gmail.com>
2022-05-14 13:15:44 +0300

    Player improved with words

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Text } from 'ink';
+import { Box, Text, Newline } from 'ink';
 import chalk from 'chalk';
 import figlet from 'figlet';
 import Song from './Song';
@@ -8,30 +8,14 @@ import { prepareSong } from '../karaokeText';
 const song = prepareSong(null);
 
 const Player = () => {
-  const [counter, setCounter] = useState(0);
-  const [index, setIndex] = useState<number>(0);
-  const lineEnds = song.lines.map((line) => line.end / 10);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCounter((previousCounter) => previousCounter + 1);
-    }, 10);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-  useEffect(() => {
-    const i = lineEnds.filter((time) => time <= counter).length;
-    setIndex(i);
-  }, [counter]);
-
   return (
     <Box flexDirection="column">
-      <Text color="green">{chalk.green(figlet.textSync('Kalle'))}</Text>
-      <Text color="red">{`Currently playing: ${song.title}, by ${song.artist}`}</Text>
-      <Song song={song} />
+      <Text>
+        <Text color="red">{`Currently playing: ${song.title}, by ${song.artist}`}</Text>
+        <Newline />
+        <Newline />
+        <Song song={song} />
+      </Text>
     </Box>
   );
 };
